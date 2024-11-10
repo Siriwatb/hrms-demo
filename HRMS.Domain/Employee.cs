40db47399cb1830820af8dc6485c24824d938e65
b-boooomm <b.boommingkid@gmail.com>
2024-11-11 00:39:18 +0700

    Add Employee class and project file for domain layer

---
+++ HRMS.Domain/Employee.cs
@@ -0,0 +1,13 @@
+namespace HRMS.Domain;
+
+public class Employee
+{
+    public string EmpNo { get; set; } = null!;
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Designation { get; set; } = null!;
+    public DateTime HireDate { get; set; }
+    public Decimal Salary { get; set; }
+    public Decimal Comm { get; set; }
+    public string DeptNo { get; set; }= null!;
+}
